f0a72488884b2e36350a9d42792e8144fc0a7ce3
rajeshpillai <pillai.rajesh@gmail.com>
2023-04-22 10:25:48 +0530

    Accept client request and respond back

--- main.go
+++ main.go
@@ -4,7 +4,24 @@ import (
   "fmt"
   "net"
   "log"
+  "time"
 )
+
+func do (conn net.Conn) {
+  
+  buf  := make([]byte, 1024)
+  _, err :=  conn.Read(buf)  // blocking call
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  //Processing
+
+  time.Sleep(1 * time.Second)  // Long running simulation
+
+  conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, Worrld!\r\n"))
+  conn.Close()
+}
 
 func main () {
   listener, err := net.Listen("tcp", ":1729")
@@ -18,4 +35,5 @@ func main () {
   }
 
   fmt.Println(conn)
+  do (conn)
 }
